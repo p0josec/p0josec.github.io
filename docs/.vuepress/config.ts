1a8663bfc7ebd5edb6939b75385d832b01a440ce
p0jo <p0josec@gmail.com>
2022-05-06 11:05:47 +0800

    delete favo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -78,15 +78,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       // },
       { text: '关于', link: '/about/' },
       { text: '友情链接', link: '/friends/' },
-      {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
-        items: [
-          { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-          // { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-          // { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-        ],
-      },
+      // {
+      //   text: '收藏',
+      //   link: '/pages/beb6c0bd8a66cea6/',
+      //   items: [
+      //     { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+      //     // { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+      //     // { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+      //   ],
+      // },
       {
         text: '索引',
         link: '/archives/',
@@ -202,6 +202,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
     //   },
     // ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
+    
   ],
 
   // 插件配置
