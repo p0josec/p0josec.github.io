d37676e001973b0974dae0a8d1637428bcebddf3
p0jo <p0josec@gmail.com>
2022-05-10 11:46:46 +0800

    close deitLinks

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -13,7 +13,7 @@ const themeConfig = {
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
-  editLinks: true, // 启用编辑
+  editLinks: false, // 启用编辑
   editLinkText: '编辑',
   docBranch: 'main',
 
